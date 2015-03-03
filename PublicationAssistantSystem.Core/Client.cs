c007e700c178075ddaa51e315127e315d0f68bad
Szymon Bartnik <szymbar099@student.polsl.pl>
2015-03-03 19:37:50 +0100

    Some progress.

---
+++ PublicationAssistantSystem.Core/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace PublicationAssistantSystem.Core
+{
+    public class Client : com.webofknowledge.search.WOKMWSAuthenticateService
+    {
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            var request = base.GetWebRequest(uri);
+            request.Headers.Add("Authorization", Test.CredentialsBased);
+            return request;
+        }
+    }
+}
