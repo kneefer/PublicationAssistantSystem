20ff5eacd8b1c4eab2207cc8a07e17c94a16b1f4
Psilo <szostek.bartek@gmail.com>
2015-05-28 23:26:21 +0200

    Added missing files.

---
+++ PublicationAssistantSystem.WebApi/Areas/Publications/Controllers/ArticlesController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using AutoMapper;
+using PublicationAssistantSystem.DAL.Context;
+using PublicationAssistantSystem.DAL.DTO.Publications;
+using PublicationAssistantSystem.DAL.Models.Publications;
+using PublicationAssistantSystem.DAL.Repositories.Specific.Interfaces;
+
+namespace PublicationAssistantSystem.WebApi.Areas.Publications.Controllers
+{
+    /// <summary>
+    /// Provides access for articles in publications repository.
+    /// </summary>
+    public class ArticlesController : ApiController
+    {
+        private readonly IPublicationAssistantContext _db;
+        private readonly IPublicationBaseRepository _publicationBaseRepository;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="db"> Db context. </param>
+        /// <param name="publicationBaseRepository"> Repository of publications. </param>
+        public ArticlesController(
+            IPublicationAssistantContext db, 
+            IPublicationBaseRepository publicationBaseRepository)
+        {
+            _db = db;
+            _publicationBaseRepository = publicationBaseRepository;
+        }
+
+        /// <summary>
+        /// Returns all publications that are articles.
+        /// </summary>
+        /// <remarks> GET: api/Publications/Articles </remarks>
+        /// <returns> All articles. </returns>
+        public IEnumerable<PublicationBaseDTO> GetAllArticles()
+        {
+            var results = _publicationBaseRepository.Get(p => p is Article).ToList();
+            var mapped = results.Select(Mapper.DynamicMap<PublicationBaseDTO>);
+            var toReturn = mapped.ToList();
+            return toReturn;
+        }
+
+        /// <summary>
+        /// Returns article with given id.
+        /// </summary>
+        /// <param name="id"> Article id. </param>
+        /// <remarks> GET: api/Publications/Articles/Id </remarks>
+        /// <returns> Article with specified id. </returns>
+        public PublicationBaseDTO GetArticle(int id)
+        {
+            var result = _publicationBaseRepository.Get(p => p is Article && p.Id == id).FirstOrDefault();
+            if (result == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            var mapped = Mapper.DynamicMap<PublicationBaseDTO>(result);
+            return mapped;
+        }
+    }
+}
