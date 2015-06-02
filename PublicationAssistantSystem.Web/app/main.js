ab99078c8753128390a161c81cf08b5075215f25
Szymon Bartnik <szymbar099@student.polsl.pl>
2015-06-02 02:05:47 +0200

    Added Web project (for angular front-end).

---
+++ PublicationAssistantSystem.Web/app/main.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+
+    function main() {
+        var vm = this;
+        vm.food = "pizza";
+    }
+
+    angular
+        .module("app")
+        .controller("Main", main);
+})();
