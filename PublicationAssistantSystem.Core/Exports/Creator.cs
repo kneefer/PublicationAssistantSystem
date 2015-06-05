d6a9ad08561873d40372f1d20dc95a14092e0497
Psilo <szostek.bartek@gmail.com>
2015-06-05 16:03:15 +0200

    Added Creator class.

---
+++ PublicationAssistantSystem.Core/Exports/Creator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PublicationAssistantSystem.DAL.DTO.Misc;
+using PublicationAssistantSystem.DAL.DTO.Publications;
+
+namespace PublicationAssistantSystem.Core.Exports
+{
+    public abstract class Creator : ICreator
+    {
+        public string Create(PublicationBaseDTO publication)
+        {
+            if (publication is BookDTO) return CreateBook((BookDTO)publication);
+            if (publication is DatasetDTO) return CreateDataset((DatasetDTO)publication);
+            if (publication is ConferencePaperDTO) return CreateConferencePaper((ConferencePaperDTO)publication);
+            if (publication is PatentDTO) return CreatePatent((PatentDTO)publication);
+            if (publication is TechnicalReportDTO) return CreateTechnicalReport((TechnicalReportDTO)publication);
+            if (publication is ThesisDTO) return CreateThesis((ThesisDTO)publication);
+
+            throw new ArgumentException("Argument publication is not valid!");
+        }
+
+        public string Create(PublicationBaseDTO publication, string journalTitle, int journalVolume)
+        {
+            if (publication is ArticleDTO)
+                return CreateArticle((ArticleDTO) publication, journalTitle, journalVolume);
+
+            throw new ArgumentException("Argument publication is not valid!");
+        }
+
+        public abstract string CreateArticle(ArticleDTO article, string journalTitle, int journalVolume);
+        public abstract string CreateBook(BookDTO book);
+        public abstract string CreateDataset(DatasetDTO dataset);
+        public abstract string CreateConferencePaper(ConferencePaperDTO book);
+        public abstract string CreatePatent(PatentDTO book);
+        public abstract string CreateTechnicalReport(TechnicalReportDTO book);
+        public abstract string CreateThesis(ThesisDTO book);
+
+        #region Helpers
+
+        protected static IList<string> GetAuthors(PublicationBaseDTO publication)
+        {
+            return publication.Employees
+                              .Select(employee => string.Format("{0} {1}", employee.FirstName, employee.LastName))
+                              .ToList();
+        }
+
+        #endregion Helpers
+    }
+}
---
+++ PublicationAssistantSystem.Core/Exports/XMLCreator.cs
@@ -0,0 +1,43 @@
+﻿using PublicationAssistantSystem.DAL.DTO.Misc;
+using PublicationAssistantSystem.DAL.DTO.Publications;
+
+namespace PublicationAssistantSystem.Core.Exports
+{
+    public class XMLCreator : Creator
+    {
+        public override string CreateArticle(ArticleDTO article, string journalTitle, int journalVolume)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string CreateBook(BookDTO book)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string CreateDataset(DatasetDTO dataset)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string CreateConferencePaper(ConferencePaperDTO book)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string CreatePatent(PatentDTO book)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string CreateTechnicalReport(TechnicalReportDTO book)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override string CreateThesis(ThesisDTO book)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
