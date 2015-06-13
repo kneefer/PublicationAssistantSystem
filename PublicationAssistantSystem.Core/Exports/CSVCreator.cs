e4d7afa678a6f289323c6df3991dc1720ee68fc9
Psilo <szostek.bartek@gmail.com>
2015-06-13 22:34:30 +0200

    Removed CsvHelper.

--- PublicationAssistantSystem.Core/Exports/CSVCreator.cs
+++ PublicationAssistantSystem.Core/Exports/CSVCreator.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using CsvHelper;
 using PublicationAssistantSystem.DAL.Models.Publications;
 
 namespace PublicationAssistantSystem.Core.Exports
@@ -8,7 +7,6 @@ namespace PublicationAssistantSystem.Core.Exports
     public class CSVCreator : Creator
     {
         private readonly StreamWriter _streamWriter;
-        private readonly CsvWriter _csvWriter;
 
         public CSVCreator()
             : this(Stream.Null)
@@ -22,7 +20,6 @@ namespace PublicationAssistantSystem.Core.Exports
                 Stream = new MemoryStream();
 
             _streamWriter = new StreamWriter(Stream);
-            _csvWriter = new CsvWriter(_streamWriter);
         }
 
         public override void CreateToStream(IEnumerable<PublicationBase> publications)
@@ -35,43 +32,36 @@ namespace PublicationAssistantSystem.Core.Exports
 
         protected override string CreateArticle(Article article)
         {
-            _csvWriter.WriteRecord(article);
             return string.Empty;
         }
 
         protected override string CreateBook(Book book)
         {
-            _csvWriter.WriteRecord(book);
             return string.Empty;
         }
 
         protected override string CreateDataset(Dataset dataset)
         {
-            _csvWriter.WriteRecord(dataset);
             return string.Empty;
         }
 
         protected override string CreateConferencePaper(ConferencePaper conferencePaper)
         {
-            _csvWriter.WriteRecord(conferencePaper);
             return string.Empty;
         }
 
         protected override string CreatePatent(Patent patent)
         {
-            _csvWriter.WriteRecord(patent);
             return string.Empty;
         }
 
         protected override string CreateTechnicalReport(TechnicalReport technicalReport)
         {
-            _csvWriter.WriteRecord(technicalReport);
             return string.Empty;
         }
 
         protected override string CreateThesis(Thesis thesis)
         {
-            _csvWriter.WriteRecord(thesis);
             return string.Empty;
         }
     }
