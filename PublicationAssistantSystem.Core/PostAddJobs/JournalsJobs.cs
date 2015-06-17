72fbadd0e8460a730135022a98fe9090c8bfb0e2
Szymon Bartnik <szymbar099@student.polsl.pl>
2015-06-17 11:11:14 +0200

    Test changes of APIs.

--- PublicationAssistantSystem.Core/PostAddJobs/JournalsJobs.cs
+++ PublicationAssistantSystem.Core/PostAddJobs/JournalsJobs.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
 using PublicationAssistantSystem.Core.SearchApiEngines;
 using PublicationAssistantSystem.DAL.Models.Misc;
 
@@ -19,21 +21,81 @@ namespace PublicationAssistantSystem.Core.PostAddJobs
         {
             var toModify = EntityToModify;
 
-            // WebOfScience existance checking
-            var results = new WebOfScienceSearchEngine()
-                .ByISBNISSN(toModify.ISSN)
-                .GetResults();
+            if (toModify.ISSN == null && toModify.eISSN == null) return;
 
-            if (results.Any())
-                _isOnWOS = true;
+            SearchWOS(toModify);
+            SearchMNiSzW(toModify);
+        }
 
-            // MNiSzW existance checking
-            results = new MniszwSearchEngine()
-                .ByISBNISSN(toModify.ISSN)
-                .GetResults();
+        private void SearchWOS(Journal toModify)
+        {
+            if (toModify.ISSN != null)
+            {
+                var results = new WebOfScienceSearchEngine()
+                    .ByISBNISSN(toModify.ISSN)
+                    .GetResults().ToList();
 
-            if (results.Any())
-                _isOnMNISZW = true;
+                if (results.Any())
+                {
+                    _isOnWOS = true;
+                    return;
+                }
+            }
+
+            if (toModify.eISSN != null)
+            {
+                var results = new WebOfScienceSearchEngine()
+                    .ByISBNISSN(toModify.eISSN)
+                    .GetResults().ToList();
+
+                if (results.Any())
+                {
+                    _isOnWOS = true;
+                }
+            }
+        }
+
+        private void SearchMNiSzW(Journal toModify)
+        {
+            var filePath = "data.csv";
+
+            using (var reader = new StreamReader(filePath))
+            {
+                if (toModify.ISSN != null)
+                {
+                    var results = new MniszwSearchEngine(reader)
+                    .ByISSN(toModify.ISSN)
+                    .GetResults().ToList();
+
+                    if (results.Count > 1)
+                        throw new Exception("Search finished with more then 1 journal result for one ISSN.");
+
+                    if (results.SingleOrDefault() != null)
+                    {
+                        _isOnMNISZW = true;
+                        // other shit
+
+                        return;
+                    }
+                }
+
+                if (toModify.eISSN != null)
+                {
+                    var results = new MniszwSearchEngine(reader)
+                    .ByISSN(toModify.eISSN)
+                    .GetResults().ToList();
+
+                    if (results.Count > 1)
+                        throw new Exception("Search finished with more then 1 journal result for one ISSN.");
+
+                    if (results.SingleOrDefault() != null)
+                    {
+                        _isOnMNISZW = true;
+                        // other shit
+
+                    }
+                }
+            }
         }
 
         protected override void SetModifications(Journal toModify)
