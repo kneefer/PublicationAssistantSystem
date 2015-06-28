c882d791ba4d254a4f9a41fdbb3498cf06bf5019
Michał Sitarz <negathio@gmail.com>
2015-06-28 19:20:52 +0200

    FE: Add publication types code

--- PublicationAssistantSystem.WebApi/Content/app/Publications/publicationFactories.js
+++ PublicationAssistantSystem.WebApi/Content/app/Publications/publicationFactories.js
@@ -4,8 +4,53 @@ var publicationsModule = angular.module("publications");
 
 publicationsModule.factory("PublicationFactory", ["$http", function ($http) {
 
+
+    // all publications code
     var getAllPublications = function () {
         return $http.get("/api/Publications/All");
+    }
+
+    var getPublication = function (publicationId) {
+        return $http.get("/api/Publications/All/" + publicationId);
+    }
+
+    var getPublicationOfEmployee = function (employeeId) {
+        return $http.get("/api/Employees/" + employeeId + "/Publications");
+    }
+
+    // conference papers code
+    var getAllConferencePapers = function () {
+        return $http.get("/api/Publications/ConferencePapers");
+    }
+
+    // technical reports code
+    var getAllTechnicalReports = function () {
+        return $http.get("/api/Publications/TechnicalReports");
+    }
+
+    // theses code
+    var getAllTheses = function () {
+        return $http.get("/api/Publications/Theses");
+    }
+
+    // datasets code
+    var getAllDatasets = function () {
+        return $http.get("/api/Publications/Datasets");
+    }
+
+    // books code
+    var getAllBooks = function () {
+        return $http.get("/api/Publications/Books");
+    }
+
+    // patents code
+    var getAllPatents = function () {
+        return $http.get("/api/Publications/Patents");
+    }
+
+    // articles code
+    var getAllArticles = function () {
+        return $http.get("/api/Publications/Articles");
     }
 
     return {
