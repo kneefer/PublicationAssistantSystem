cfcf9b2672bd2e10fdf1b4b38fa1508358249fcc
Kamil Steuer <kamil.steuer@autea.pl>
2015-07-07 11:53:05 +0200

    Added help site

---
+++ PublicationAssistantSystem.WebApi/Content/app/Docs/docs.js
@@ -0,0 +1,10 @@
+﻿"use strict";
+
+var docsModule = angular.module("docs");
+
+docsModule.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+        .when("/Docs", {
+            templateUrl: "Content/app/Docs/docs.html"
+        });
+}]);
--- PublicationAssistantSystem.WebApi/Content/app/app.module.js
+++ PublicationAssistantSystem.WebApi/Content/app/app.module.js
@@ -1,13 +1,14 @@
 ﻿(function () {
     "use strict";
     
-    angular.module("app", ["ngRoute", "publications", "faculties", "institutes", "divisions", "employees", "journals", "journalEditions"]);
+    angular.module("app", ["ngRoute", "publications", "faculties", "institutes", "divisions", "employees", "journals", "journalEditions", "docs"]);
     angular.module("publications", ["ngRoute"]);
     angular.module("faculties", ["ngRoute"]);
     angular.module("institutes", ["ngRoute"]);
     angular.module("divisions", ["ngRoute"]);
     angular.module("employees", ["ngRoute"]);
-    angular.module("journals", ["ngRoute"])
-    angular.module("journalEditions", ["ngRoute"])
+    angular.module("journals", ["ngRoute"]);
+    angular.module("journalEditions", ["ngRoute"]);
+    angular.module("docs", ["ngRoute"]);
 
 })();
